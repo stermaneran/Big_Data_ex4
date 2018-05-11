c153590d11ec0c70d72c41d1b7ea5718aba2941e
Unknown <stermaneran@gmail.com>
2018-05-11 13:15:37 +0300

    Auto stash before merge of "eran-work" and "origin/damir-work"
    redirect to home page

--- app.js
+++ app.js
@@ -21,24 +21,24 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
@@ -56,11 +56,9 @@ mongoose.connect(mongoDB, {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error!\n'));
 
-
-app.get('*', function(req, res){
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + "/index.html"));
 });
-
 
 // var load_data = require('./load_data');
 // var csvheaders ={
