5cd382e0adb5d0fded35b1534bf10c8c5e4cd557
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2018-05-11 13:02:51 +0300

    - Some deletions and modifications in front end

--- public/js_codes/wizerApp.js
+++ public/js_codes/wizerApp.js
@@ -10,41 +10,6 @@ wizerApp.config(function ($routeProvider, $locationProvider) {
             controller: 'homeController'
         })
 
-        .when('/profile/:id', {
-            templateUrl: '../pages/profile.html',
-            controller: 'profileController'
-        })
-
-        .when('/students/search-by-name/:fname/:lname', {
-            templateUrl: '../pages/searchbyname.html',
-            controller: 'getProfilesController'
-        })
-
-        .when('/students/search-by-name/:fname', {
-            templateUrl: '../pages/searchbyname.html',
-            controller: 'getProfilesController'
-        })
-
-        .when('/courses',{
-            templateUrl:'../pages/courses.html',
-            controller:'courseController'
-        })
-
-        .when('/courses/search-by-name/:course_name', {
-            templateUrl: '../pages/courses.html',
-            controller: 'courseController'
-        })
-
-        .when('/edit-profile', {
-            templateUrl: '../pages/edit_profile.html',
-            controller: 'editProfileController'
-        })
-
-        .when('/sessions/', {
-            templateUrl: '../pages/session.html',
-            controller: 'sessionController'
-        })
-
     .otherwise({ redirectTo: '/'});
 
 
