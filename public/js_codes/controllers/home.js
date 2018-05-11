acadf7c9c787e1615a473aaba75f5aa39400a71f
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2018-05-11 14:02:26 +0300

    - Forms working

--- public/js_codes/controllers/home.js
+++ public/js_codes/controllers/home.js
@@ -1,5 +1,25 @@
-wizerApp.controller('homeController', ['$scope', '$http', 'SessionService', function ($scope, $http, SessionService) {
+wizerApp.controller('homeController', ['$scope', '$http', 'ProfileService', function ($scope, $http, ProfileService) {
 
+    $scope.entry = {};
+
+
+    $scope.searchByAge = function() {
+        ProfileService.getByAge($scope.entry.age)
+            .then(function (data) {
+                console.log(JSON.stringify(data));
+            });
+    };
+
+
+    $scope.searchByGender = function() {
+        $http.get('/queries/get-by-sex?gender=' + $scope.entry.gender)
+            .then(function (data) {
+                console.log(JSON.stringify(data))
+            })
+            .catch(function () {
+                console.log("Caught error in get-by-age");
+            });
+    };
 
 
 }]);
